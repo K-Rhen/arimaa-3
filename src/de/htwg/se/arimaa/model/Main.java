475c66f01ee3873fb5f995b1274abac13e146264
Michael Lihs <michaellihs@gmail.com>
2016-04-01 08:43:59 +0200

    Test3

--- src/de/htwg/se/arimaa/model/Main.java
+++ src/de/htwg/se/arimaa/model/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for(int i = 0;i < 3;++i)
+			System.out.println("Hallo Michael");
 		System.out.print("Hallo Martin");
 		System.out.print("Hallo Martin");
 		
