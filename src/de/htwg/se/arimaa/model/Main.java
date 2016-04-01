64309f0a5211b504596dae36d48bef613021351b
Michael Lihs <michaellihs@gmail.com>
2016-04-01 12:55:20 +0200

    build first class structure

--- src/de/htwg/se/arimaa/model/Main.java
+++ src/de/htwg/se/arimaa/model/Main.java
@@ -3,12 +3,7 @@ package de.htwg.se.arimaa.model;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		System.out.println("hasldfj");
-		
-		
+		Arimaa arima = new Arimaa();
 	}
 
 }
---
+++ src/de/htwg/se/arimaa/test/GuidelineTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package de.htwg.se.arimaa.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class GuidelineTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
