f13b359b0a2c9ed2afe120f75f038cc3a32b5385
MartinLei <malei@htwg-konstanz.de>
2016-04-19 11:48:53 +0200

    First Figur init

--- src/de/htwg/se/arimaa/model/Character.java
+++ src/de/htwg/se/arimaa/model/Character.java
@@ -1,5 +1,17 @@
 package de.htwg.se.arimaa.model;
 
 public class Character {
-
+		private static class Position{
+			private int x;
+			private int y;
+			
+			public Position(int x, int y){
+				this.x = x;
+				this.y = y;
+			}
+		}
+		
+		private Position pos;
+		
+		
 }
