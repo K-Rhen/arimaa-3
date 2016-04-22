e35c004b5a34da214acdc77b39a68f4f002b8aeb
Martin Lei <malei@htwg-konstanz.de>
2016-04-22 12:56:41 +0200

    c2

---
+++ test/de/htwg/se/arimaa/model/PitchTest.java
@@ -0,0 +1,18 @@
+package de.htwg.se.arimaa.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PitchTest {
+	Pitch pitch;
+	
+	@Before
+	public void setUp() throws Exception {
+		pitch = new Pitch("T1","T2");
+	}
+	
+	@Test
+	public void testToString(){
+  //TODO
+	}
+}
