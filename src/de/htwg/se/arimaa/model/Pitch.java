e814ec33a9ca08ddbf9d692d02a809b727f9328d
Martin Lei <malei@htwg-konstanz.de>
2016-04-22 09:30:52 +0200

    first init of the Pitch without a testclass

--- src/de/htwg/se/arimaa/model/Pitch.java
+++ src/de/htwg/se/arimaa/model/Pitch.java
@@ -1,5 +1,22 @@
 package de.htwg.se.arimaa.model;
 
 public class Pitch {
+	
+  private Player p1;
+  private Player p2;
+  private static final int PITCHSIZE = 7;
+
+  public Pitch (String player1Name, String player2Name){
+	  
+	  
+  }
+  
+ @Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+  
+  
 
 }
