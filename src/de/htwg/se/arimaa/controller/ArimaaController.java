b94ec00f764bd5a77e8f1588127110ec7fd90c80
MichaelLihsHtwg <michaellihs@gmail.com>
2016-05-28 21:21:30 +0200

    working on start

--- src/de/htwg/se/arimaa/controller/ArimaaController.java
+++ src/de/htwg/se/arimaa/controller/ArimaaController.java
@@ -12,20 +12,15 @@ import de.htwg.se.arimaa.util.character.Position;
 public class ArimaaController {
 	private Pitch pitch;
 	private Rules rules;
-
+	
+	private List<Character> figures1 = new ArrayList<>();
+	private List<Character> figures2 = new ArrayList<>();
 	public ArimaaController() {
 		initPitchPlayer();
 		rules = new Rules(pitch);
 	}
 
 	private void initPitchPlayer() {
-		List<Character> figures1 = new ArrayList<>();
-		figures1.add(new Character(new Position(0, 0), CHARAKTER_NAME.R));
-		
-
-		List<Character> figures2 = new ArrayList<>();
-		figures2.add(new Character(new Position(0, 7), CHARAKTER_NAME.r));
-		
 		pitch = new Pitch("Player1", "Player2", figures1, figures2);
 	}
 
@@ -33,14 +28,34 @@ public class ArimaaController {
 		System.out.println(pitch.toString());
 	}
 	
-	public boolean setFigure(int player, Character figure, Position set){
-		return false;
+	public void setFigure(String player, String string){
+		String figurename = null;
+		String position = null;
+		String[] parts = string.split(" ");
+		parts[0] = figurename; 
+		parts[1] = position;
+		Position pos = new Position(readposx(position), readposy(position));
+		CHARAKTER_NAME figur = readname(figurename);
+		List<Character> list = new ArrayList<Character>();
+		Character character = new Character(pos,figur);
+		figures1.add(character);
 		
 	}
 
 	public boolean moveFigur(int player, Position from, Position to){
 		
 		return false;
+	}
+	
+	private int readposx(String posx){
+		return 0;
+	}
+	private int readposy(String posy){
+		return 0;
+	}
+	private CHARAKTER_NAME readname(String name){
+		return null;
+		
 	}
 	
 }
--- src/de/htwg/se/arimaa/view/TextUI.java
+++ src/de/htwg/se/arimaa/view/TextUI.java
@@ -1,6 +1,5 @@
 package de.htwg.se.arimaa.view;
 
-
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,49 +10,68 @@ import de.htwg.se.arimaa.model.Player;
 import java.lang.System;
 
 public class TextUI {
-	
+
 	Scanner EINGABE = new Scanner(System.in);
 	ArimaaController controller = new ArimaaController();
-	
-	private Player p1;
-	private List<Character> figures1;
-	private Player p2;
-	private List<Character> figures2;
-	
-	
-	public void start(){
-			
+
+	String player1;
+	String player2;
+
+	public void start() {
+
 		System.out.println("Hallo. Willkommen zu Arimaa.");
 		System.out.println("Spieler eins bitte geb deinen Namen ein.\n");
-		p1 = new Player(EINGABE.nextLine(), figures1);
-		System.out.println("\nHallo " + p1.getPlayerName() + "\n");
+		player1 = EINGABE.nextLine();
+		System.out.println("\nHallo " + player1 + "\n");
 		System.out.println("Spieler zwei bitte geb deinen Namen ein.\n");
-		p2 = new Player(EINGABE.nextLine(), figures2);
-		System.out.println("\nHallo " + p2.getPlayerName() + "\n");
+		player2 = EINGABE.nextLine();
+		System.out.println("\nHallo " + player2 + "\n");
+
+		System.out.println(player1 + " bitte setze deine Figuren,");
+		System.out.println("indem du z.B. \"R a1\" eingibst, oder \"egal\" "
+				+ " für die Standartbesetzung.");
+		if (EINGABE.nextLine().equals("egal")) {
+			System.out.println("TODO");
+		} else {
+			for (int i = 0; i < 16; ++i) {
+				System.out.println("Du kannst noch " + (16 - i)
+						+ " Figuren setzen");
+				controller.setFigure("p1", EINGABE.nextLine());
+			}
+		}
 		
-		System.out.println(p1.getPlayerName() + " bitte setze deine Figuren,");
-		System.out.println("indem du z.B. \"R a1\" eingibst.");
-		
+		System.out.println(player2 + " bitte setze deine Figuren,");
+		System.out.println("indem du z.B. \"r h1\" eingibst, oder \"egal\" "
+				+ " für die Standartbesetzung.");
+		if (EINGABE.nextLine().equals("egal")) {
+			System.out.println("TODO");
+		} else {
+			for (int i = 0; i < 16; ++i) {
+				System.out.println("Du kannst noch " + (16 - i)
+						+ " Figuren setzen");
+				controller.setFigure("p1", EINGABE.nextLine());
+			}
+		}
+
 	}
-	
-	public void run(){
-		
-		while(true){
-			
+
+	public void run() {
+
+		while (true) {
+
 			controller.ShowPitch();
 			System.out.printf("Mache einen Zug <z.B. mit a3-a4>\n");
 			System.out.println("help fuer Hilfe\n");
-			
+
 			String eingabe = EINGABE.nextLine();
 
-			if (eingabe.equals("exit")){
+			if (eingabe.equals("exit")) {
 				return;
 			}
-			if(eingabe.equals("help")){
+			if (eingabe.equals("help")) {
 				System.out.println("TODO");
 			}
-			
-			
+
 		}
 	}
 
