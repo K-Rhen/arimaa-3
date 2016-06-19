49256f991a3cdaa4ede365a1e473de6445bb4e64
MartinLei <malei@htwg-konstanz.de>
2016-06-19 15:57:10 +0200

    add google guice

--- src/de/htwg/se/arimaa/arimaa/ArimaaModule.java
+++ src/de/htwg/se/arimaa/arimaa/ArimaaModule.java
@@ -1,20 +1,20 @@
 package de.htwg.se.arimaa.arimaa;
 import com.google.inject.AbstractModule;
 
-import src.de.htwg.sudoku.IGridFactory;
-import src.de.htwg.sudoku.ISudokuController;
-import src.de.htwg.sudoku.de;
+//import src.de.htwg.sudoku.IGridFactory;
+//import src.de.htwg.sudoku.ISudokuController;
+//import src.de.htwg.sudoku.de;
 
 public class ArimaaModule extends AbstractModule{
 
 	@Override
 	protected void configure() {
-	    bind(IGridFactory.class)
-        .to(de.htwg.sudoku.model.impl.GridFactory.class);
-	    
-	    bind(ISudokuController.class).to(
-        de.htwg.sudoku.controller.logwrapper.SudokuController.class);
-		
+//	    bind(IGridFactory.class)
+//        .to(de.htwg.sudoku.model.impl.GridFactory.class);
+//	    
+//	    bind(ISudokuController.class).to(
+//        de.htwg.sudoku.controller.logwrapper.SudokuController.class);
+//		
 	}
 
 }
