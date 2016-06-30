65a7bd94c15c54d5cd8e0cc5fdd2c0b8f1182794
MartinLei <malei@htwg-konstanz.de>
2016-07-01 00:00:52 +0200

    fix

--- test/de/htwg/se/arimaa/controller/ArimaaControllerTest.java
+++ test/de/htwg/se/arimaa/controller/ArimaaControllerTest.java
@@ -1,46 +1,23 @@
 package de.htwg.se.arimaa.controller;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
+import org.junit.Test;
 
-import junit.framework.TestCase;
 
+public class ArimaaControllerTest {
 
-public class ArimaaControllerTest extends TestCase{
-
-	IArimaaController controller;
-
+	private IArimaaController controller;
 	@Before
 	public void setUp() throws Exception {
 		controller = IArimaaControllerFactory.getInstance();
 	}
 
-//	@Test
-//	void testmoveFigur() {
-		// success move
-//		Position from = new Position(0, 1);
-//		Position to = new Position(0, 2);
-//		assertFalse(controller.getMoveCounter() == 4 );
-//		assertTrue(controller.moveFigur(1, from, to));
-//
-//		// same position
-//		from = new Position(0, 0);
-//		to = new Position(0, 0);
-//		assertFalse(controller.moveFigur(1, from, to));
-//
-//		// no free cell
-//		from = new Position(0, 0);
-//		to = new Position(1, 0);
-//		assertFalse(controller.moveFigur(1, from, to));
-//
-//		// more than one move in x-axis
-//		from = new Position(1, 0);
-//		to = new Position(3, 0);
-//		assertFalse(controller.moveFigur(1, from, to));
-//
-//		// more than one move in y-axis
-//		from = new Position(0, 2);
-//		to = new Position(2, 2);
-//		assertFalse(controller.moveFigur(1, from, to));
-//
-//	}
+	@Test
+	public void testgetActualPlayer() {
+		int ap = controller.getActualPlayer();
+		assertTrue(ap == 1);
+
+	}
 }
