83d02b752a91d28f76c8ea4996aef97bdf716ecf
Martin Lei <malei@htwg-konstanz.de>
2018-08-19 23:30:58 +0200

    format code

--- src/main/Arimaa.java
+++ src/main/Arimaa.java
@@ -10,54 +10,52 @@ import controller.IArimaaController;
 import java.util.Scanner;
 
 public class Arimaa {
-	private TextUI tui;
-	private ArimaaFrame gui;
-	protected IArimaaController controller;
-	private static Arimaa instance = null;
+    private TextUI tui;
+    private ArimaaFrame gui;
+    protected IArimaaController controller;
+    private static Arimaa instance = null;
 
-	private Arimaa() {
-		Injector injector = Guice.createInjector(new ArimaaModule());
+    private Arimaa() {
+        Injector injector = Guice.createInjector(new ArimaaModule());
 
-		controller = injector.getInstance(IArimaaController.class);
-		tui = new TextUI(controller);
-		gui = new ArimaaFrame(controller);
+        controller = injector.getInstance(IArimaaController.class);
+        tui = new TextUI(controller);
+        gui = new ArimaaFrame(controller);
 
-		// init new game
-		controller.createNewGame();
-
-
+        // init new game
+        controller.createNewGame();
 
 
-	}
+    }
 
-	public TextUI getTui() {
-		return tui;
-	}
+    public TextUI getTui() {
+        return tui;
+    }
 
 
-	public ArimaaFrame getGui() {
-		return gui;
-	}
+    public ArimaaFrame getGui() {
+        return gui;
+    }
 
-	private static Arimaa getInstance() {
-		if (instance == null) {
-			instance = new Arimaa();
-		}
-		return instance;
-	}
+    private static Arimaa getInstance() {
+        if (instance == null) {
+            instance = new Arimaa();
+        }
+        return instance;
+    }
 
-	private boolean exit() {
-		return controller.getGameStatus().equals(GameStatus.EXIT);
-	}
+    private boolean exit() {
+        return controller.getGameStatus().equals(GameStatus.EXIT);
+    }
 
-	public static void main(final String[] args) {
-		Arimaa game = Arimaa.getInstance();
+    public static void main(final String[] args) {
+        Arimaa game = Arimaa.getInstance();
 
-		Scanner scanner = new Scanner(System.in);
-		while (!game.exit()) {
-			game.tui.processInputLine(scanner.next());
-		}
+        Scanner scanner = new Scanner(System.in);
+        while (!game.exit()) {
+            game.tui.processInputLine(scanner.next());
+        }
 
-	}
+    }
 
 }
