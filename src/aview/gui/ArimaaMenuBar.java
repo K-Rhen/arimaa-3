552929c2978c324262bc0a5da8cae94b3b5d90d8
Martin Lei <malei@htwg-konstanz.de>
2018-08-20 00:04:03 +0200

    use lambdas

--- src/aview/gui/ArimaaMenuBar.java
+++ src/aview/gui/ArimaaMenuBar.java
@@ -11,86 +11,76 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
 public class ArimaaMenuBar extends JMenuBar {
-	private static final Logger LOGGER = LogManager.getLogger(ArimaaMenuBar.class.getName());
+    private static final Logger LOGGER = LogManager.getLogger(ArimaaMenuBar.class.getName());
 
-	private IArimaaController controller;
+    private IArimaaController controller;
 
-	public ArimaaMenuBar(IArimaaController controller) {
-		this.controller = controller;
-		createFileMenu(controller);
-		createInfoMenu();
-	}
+    public ArimaaMenuBar(IArimaaController controller) {
+        this.controller = controller;
+        createFileMenu(controller);
+        createInfoMenu();
+    }
 
-	private void createFileMenu(IArimaaController controller) {
-		JMenu fileMenu = new JMenu("File");
-		fileMenu.setMnemonic(KeyEvent.VK_F);
+    private void createFileMenu(IArimaaController controller) {
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
 
-		JMenuItem newMenuItem = new JMenuItem("new game");
-		newMenuItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
+        JMenuItem newMenuItem = new JMenuItem("new game");
+        newMenuItem.addActionListener(al -> {
 
-				String[] yesNoOptions = { "Yes", "No" };
-				int n = JOptionPane.showOptionDialog(null, "Do you want to start a new game?", "New game?",
-						JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, yesNoOptions, yesNoOptions[0]);
+            String[] yesNoOptions = {"Yes", "No"};
+            int n = JOptionPane.showOptionDialog(null, "Do you want to start a new game?", "New game?",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, yesNoOptions, yesNoOptions[0]);
 
-				if (n == JOptionPane.YES_OPTION) {
-					controller.createNewGame();
-				}
+            if (n == JOptionPane.YES_OPTION) {
+                controller.createNewGame();
+            }
+        });
+        newMenuItem.setMnemonic(KeyEvent.VK_N);
+        newMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+        fileMenu.add(newMenuItem);
 
-			}
-		});
-		newMenuItem.setMnemonic(KeyEvent.VK_N);
-		newMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
-		fileMenu.add(newMenuItem);
+        JMenuItem quitMenuItem = new JMenuItem("quit");
+        quitMenuItem.addActionListener(al -> {
+            controller.quitGame();
+        });
+        quitMenuItem.setMnemonic(KeyEvent.VK_Q);
+        quitMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_DOWN_MASK));
+        fileMenu.add(quitMenuItem);
 
-		JMenuItem quitMenuItem = new JMenuItem("quit");
-		quitMenuItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				controller.quitGame();
-			}
-		});
-		quitMenuItem.setMnemonic(KeyEvent.VK_Q);
-		quitMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_DOWN_MASK));
-		fileMenu.add(quitMenuItem);
+        this.add(fileMenu);
+    }
 
-		this.add(fileMenu);
-	}
+    private void createInfoMenu() {
+        JMenu infoMenu = new JMenu("Info");
+        infoMenu.setMnemonic(KeyEvent.VK_F);
 
-	private void createInfoMenu() {
-		JMenu infoMenu = new JMenu("Info");
-		infoMenu.setMnemonic(KeyEvent.VK_F);
+        JMenuItem helpMenuItem = new JMenuItem("Help");
+        helpMenuItem.setMnemonic(KeyEvent.VK_H);
+        helpMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, InputEvent.CTRL_DOWN_MASK));
+        helpMenuItem.addActionListener(al -> {
+            ImageIcon icon = createImageIcon("/img/GoldElephant.png");
 
-		JMenuItem helpMenuItem = new JMenuItem("Help");
-		helpMenuItem.setMnemonic(KeyEvent.VK_H);
-		helpMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, InputEvent.CTRL_DOWN_MASK));
-		helpMenuItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				ImageIcon icon = createImageIcon("/img/GoldElephant.png");
+            JOptionPane.showMessageDialog(null,
+                    "This project is an implementation of the arimaa game.\n\n"
+                            + "It was created for the lecture Software Engineering\n"
+                            + "at the University of Applied Science Konstanz, Germany.\n\n"
+                            + "For more information about arimaa,\n" + "          go to http://arimaa.com",
+                    "HTWG Arimaa, Germany 2016", JOptionPane.INFORMATION_MESSAGE, icon);
+        });
 
-				JOptionPane.showMessageDialog(null,
-						"This project is an implementation of the arimaa game.\n\n"
-								+ "It was created for the lecture Software Engineering\n"
-								+ "at the University of Applied Science Konstanz, Germany.\n\n"
-								+ "For more information about arimaa,\n" + "          go to http://arimaa.com",
-						"HTWG Arimaa, Germany 2016", JOptionPane.INFORMATION_MESSAGE, icon);
-			}
-		});
+        infoMenu.add(helpMenuItem);
+        this.add(infoMenu);
+    }
 
-		infoMenu.add(helpMenuItem);
-		this.add(infoMenu);
-	}
-
-	private ImageIcon createImageIcon(String path) {
-		java.net.URL imgURL = getClass().getResource(path);
-		if (imgURL != null) {
-			return new ImageIcon(imgURL);
-		} else {
-			LOGGER.error("Images: " + path + "not found");
-			return null;
-		}
-	}
+    private ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            LOGGER.error("Images: " + path + "not found");
+            return null;
+        }
+    }
 
 }
