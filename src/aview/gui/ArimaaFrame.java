3600fe9a373ab2fa39e27d7b5827cd86c66fd043
Martin Lei <malei@htwg-konstanz.de>
2018-08-19 23:48:10 +0200

    format code

--- src/aview/gui/ArimaaFrame.java
+++ src/aview/gui/ArimaaFrame.java
@@ -13,77 +13,77 @@ import java.awt.event.WindowEvent;
 
 public class ArimaaFrame extends JFrame implements IObserver {
 
-	private static final int DEFAULT_Y = 520;
-	private static final int DEFAULT_X = 720;
+    private static final int DEFAULT_Y = 520;
+    private static final int DEFAULT_X = 720;
 
-	private Container pane;
-	private IArimaaController controller;
+    private Container pane;
+    private IArimaaController controller;
 
-	@Inject
-	public ArimaaFrame(final IArimaaController controller) {
-		this.controller = controller;
-		controller.addObserver(this);
+    @Inject
+    public ArimaaFrame(final IArimaaController controller) {
+        this.controller = controller;
+        controller.addObserver(this);
 
-		this.setTitle("HTWG Arimaa");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(DEFAULT_X, DEFAULT_Y);
-		this.setJMenuBar(new ArimaaMenuBar(controller));
-		pane = getContentPane();
-		pane.setLayout(new BorderLayout());
+        this.setTitle("HTWG Arimaa");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(DEFAULT_X, DEFAULT_Y);
+        this.setJMenuBar(new ArimaaMenuBar(controller));
+        pane = getContentPane();
+        pane.setLayout(new BorderLayout());
 
-		constructArimaaPane(controller);
+        constructArimaaPane(controller);
 
-		// Closing window handler
-		this.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent we) {
-				controller.quitGame();
-			}
-		});
-	}
+        // Closing window handler
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent we) {
+                controller.quitGame();
+            }
+        });
+    }
 
-	private void constructArimaaPane(IArimaaController controller) {
-		JPanel centerPanel = new JPanel();
-		pane.add(centerPanel, BorderLayout.CENTER);
+    private void constructArimaaPane(IArimaaController controller) {
+        JPanel centerPanel = new JPanel();
+        pane.add(centerPanel, BorderLayout.CENTER);
 
-		JPanel leftPanel = new JPanel(new GridLayout(1, 0));
+        JPanel leftPanel = new JPanel(new GridLayout(1, 0));
 
-		leftPanel.setPreferredSize(new Dimension(440, 440));
-		centerPanel.add(leftPanel);
+        leftPanel.setPreferredSize(new Dimension(440, 440));
+        centerPanel.add(leftPanel);
 
-		PitchPanel pitchPanel = new PitchPanel(controller);
-		leftPanel.add(pitchPanel);
+        PitchPanel pitchPanel = new PitchPanel(controller);
+        leftPanel.add(pitchPanel);
 
-		JPanel rightPanel = new JPanel();
-		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
-		centerPanel.add(rightPanel);
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
+        centerPanel.add(rightPanel);
 
-		InfoPanel infoPanel = new InfoPanel(controller);
-		rightPanel.add(infoPanel);
+        InfoPanel infoPanel = new InfoPanel(controller);
+        rightPanel.add(infoPanel);
 
-		MoveHistoryPanel moveStatusPanel = new MoveHistoryPanel(controller);
-		rightPanel.add(moveStatusPanel);
+        MoveHistoryPanel moveStatusPanel = new MoveHistoryPanel(controller);
+        rightPanel.add(moveStatusPanel);
 
-		ButtonPanel buttonPanel = new ButtonPanel(controller);
-		rightPanel.add(buttonPanel);
+        ButtonPanel buttonPanel = new ButtonPanel(controller);
+        rightPanel.add(buttonPanel);
 
-		StatusPanel statusPanel = new StatusPanel(controller);
-		pane.add(statusPanel, BorderLayout.SOUTH);
+        StatusPanel statusPanel = new StatusPanel(controller);
+        pane.add(statusPanel, BorderLayout.SOUTH);
 
-		setVisible(true);
-		repaint();
-	}
+        setVisible(true);
+        repaint();
+    }
 
-	@Override
-	public void update(Event e) {
-	    GameStatus gs = controller.getGameStatus();
-		if (gs.equals(GameStatus.EXIT)) {
-			this.setVisible(false);
-			this.dispose();
-			return;
-		}
+    @Override
+    public void update(Event e) {
+        GameStatus gs = controller.getGameStatus();
+        if (gs.equals(GameStatus.EXIT)) {
+            this.setVisible(false);
+            this.dispose();
+            return;
+        }
 
-		repaint();
-	}
+        repaint();
+    }
 
 }
