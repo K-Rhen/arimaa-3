a3e0b2adfed2582c57df9c5051d2ed87e8e7cbaf
Martin Lei <malei@htwg-konstanz.de>
2018-08-20 00:11:28 +0200

    remove unused imports

--- src/aview/gui/ArimaaMenuBar.java
+++ src/aview/gui/ArimaaMenuBar.java
@@ -5,8 +5,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
